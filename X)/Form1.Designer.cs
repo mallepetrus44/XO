1973e78e33d65a8905a214e06a1ca7324ce2c7e2
mallepetrus44 <42939378+mallepetrus44@users.noreply.github.com>
2018-12-21 12:53:52 +0100

    Add project files.

---
+++ X)/Form1.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace X_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.panel7 = new System.Windows.Forms.Panel();
+            this.panel8 = new System.Windows.Forms.Panel();
+            this.panel9 = new System.Windows.Forms.Panel();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Location = new System.Drawing.Point(112, 67);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Speler 1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Location = new System.Drawing.Point(224, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Speler 2";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Transparent;
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel1.Location = new System.Drawing.Point(71, 97);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(75, 75);
+            this.panel1.TabIndex = 11;
+            this.panel1.Click += new System.EventHandler(this.PANEL_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Transparent;
+            this.panel2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel2.Location = new System.Drawing.Point(152, 97);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(75, 75);
+            this.panel2.TabIndex = 0;
+            this.panel2.Click += new System.EventHandler(this.PANEL_Click);
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.Transparent;
+            this.panel3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel3.Location = new System.Drawing.Point(233, 97);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(75, 75);
+            this.panel3.TabIndex = 0;
+            this.panel3.Click += new System.EventHandler(this.PANEL_Click);
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.Transparent;
+            this.panel4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel4.Location = new System.Drawing.Point(71, 178);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(75, 75);
+            this.panel4.TabIndex = 0;
+            this.panel4.Click += new System.EventHandler(this.PANEL_Click);
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.Color.Transparent;
+            this.panel5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel5.Location = new System.Drawing.Point(152, 178);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(75, 75);
+            this.panel5.TabIndex = 0;
+            this.panel5.Click += new System.EventHandler(this.PANEL_Click);
+            // 
+            // panel6
+            // 
+            this.panel6.BackColor = System.Drawing.Color.Transparent;
+            this.panel6.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel6.Location = new System.Drawing.Point(233, 178);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(75, 75);
+            this.panel6.TabIndex = 0;
+            this.panel6.Click += new System.EventHandler(this.PANEL_Click);
+            // 
+            // panel7
+            // 
+            this.panel7.BackColor = System.Drawing.Color.Transparent;
+            this.panel7.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel7.Location = new System.Drawing.Point(71, 259);
+            this.panel7.Name = "panel7";
+            this.panel7.Size = new System.Drawing.Size(75, 75);
+            this.panel7.TabIndex = 0;
+            this.panel7.Click += new System.EventHandler(this.PANEL_Click);
+            // 
+            // panel8
+            // 
+            this.panel8.BackColor = System.Drawing.Color.Transparent;
+            this.panel8.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel8.Location = new System.Drawing.Point(152, 259);
+            this.panel8.Name = "panel8";
+            this.panel8.Size = new System.Drawing.Size(75, 75);
+            this.panel8.TabIndex = 0;
+            this.panel8.Click += new System.EventHandler(this.PANEL_Click);
+            // 
+            // panel9
+            // 
+            this.panel9.BackColor = System.Drawing.Color.Transparent;
+            this.panel9.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel9.Location = new System.Drawing.Point(233, 259);
+            this.panel9.Name = "panel9";
+            this.panel9.Size = new System.Drawing.Size(75, 75);
+            this.panel9.TabIndex = 0;
+            this.panel9.Click += new System.EventHandler(this.PANEL_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.Image = global::X_.Properties.Resources.o;
+            this.pictureBox2.Location = new System.Drawing.Point(220, 15);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox2.TabIndex = 13;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = global::X_.Properties.Resources.x1;
+            this.pictureBox1.Location = new System.Drawing.Point(108, 16);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox1.TabIndex = 12;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::X_.Properties.Resources.back_xo;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(384, 361);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel5);
+            this.Controls.Add(this.panel6);
+            this.Controls.Add(this.panel7);
+            this.Controls.Add(this.panel8);
+            this.Controls.Add(this.panel9);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.Panel panel7;
+        private System.Windows.Forms.Panel panel8;
+        private System.Windows.Forms.Panel panel9;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ X)/Form1.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace X_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public bool turn_player; // player 1= false, player 2= true
+
+        public Image S1 = Properties.Resources.x1; //image player 1
+        public Image S2 = Properties.Resources.o; //image player 2
+
+        public Panel selected_panel;
+        public List<String> panel_images = new List<String>();
+
+        string result; // result string <- goh
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            turn_player = false;
+            panel_images.Clear();
+        }
+
+        private void check_win()
+        {
+            get_all_info();
+
+            // player 1
+            if (panel1.BackgroundImage == S1 &&
+                panel2.BackgroundImage == S1 &&
+                panel3.BackgroundImage == S1 ||
+
+                panel4.BackgroundImage == S1 &&
+                panel5.BackgroundImage == S1 &&
+                panel6.BackgroundImage == S1 ||
+
+                panel7.BackgroundImage == S1 &&
+                panel8.BackgroundImage == S1 &&
+                panel9.BackgroundImage == S1 ||
+
+                panel1.BackgroundImage == S1 &&
+                panel5.BackgroundImage == S1 &&
+                panel9.BackgroundImage == S1 ||
+
+                panel3.BackgroundImage == S1 &&
+                panel5.BackgroundImage == S1 &&
+                panel7.BackgroundImage == S1 ||
+
+                panel1.BackgroundImage == S1 &&
+                panel4.BackgroundImage == S1 &&
+                panel7.BackgroundImage == S1 ||
+
+                panel2.BackgroundImage == S1 &&
+                panel5.BackgroundImage == S1 &&
+                panel8.BackgroundImage == S1 ||
+
+                panel3.BackgroundImage == S1 &&
+                panel6.BackgroundImage == S1 &&
+                panel9.BackgroundImage == S1)
+            {
+                MessageBox.Show("WIN player 1");
+                start_new();
+            }
+
+            // player 2
+            if (panel1.BackgroundImage == S2 &&
+                panel2.BackgroundImage == S2 &&
+                panel3.BackgroundImage == S2 ||
+
+                panel4.BackgroundImage == S2 &&
+                panel5.BackgroundImage == S2 &&
+                panel6.BackgroundImage == S2 ||
+
+                panel7.BackgroundImage == S2 &&
+                panel8.BackgroundImage == S2 &&
+                panel9.BackgroundImage == S2 ||
+
+                panel1.BackgroundImage == S2 &&
+                panel5.BackgroundImage == S2 &&
+                panel9.BackgroundImage == S2 ||
+
+                panel3.BackgroundImage == S2 &&
+                panel5.BackgroundImage == S2 &&
+                panel7.BackgroundImage == S2 ||
+
+                panel1.BackgroundImage == S2 &&
+                panel4.BackgroundImage == S2 &&
+                panel7.BackgroundImage == S2 ||
+
+                panel2.BackgroundImage == S2 &&
+                panel5.BackgroundImage == S2 &&
+                panel8.BackgroundImage == S2 ||
+
+                panel3.BackgroundImage == S2 &&
+                panel6.BackgroundImage == S2 &&
+                panel9.BackgroundImage == S2)
+            {
+                MessageBox.Show("WIN player 2");
+                start_new();
+            }
+
+        }
+
+        private void start_new()
+        {
+            foreach (Panel p in this.Controls.OfType<Panel>())
+            {
+                p.BackgroundImage = null;
+            }
+            turn_player = false;
+            panel_images.Clear();
+
+
+        }
+
+        private void get_all_info()
+        {
+            panel_images.Clear();
+
+            foreach (Panel p in this.Controls.OfType<Panel>())
+            {
+                if (p.BackgroundImage != null)
+                {
+                    if (p.BackgroundImage == S1)
+                    {
+                        panel_images.Add(p.Name.ToString() + " - S1");
+                    }
+                    if (p.BackgroundImage == S2)
+                    {
+                        panel_images.Add(p.Name.ToString() + " - S2");
+                    }
+                }
+            }
+        }
+        private void PANEL_Click(object sender, EventArgs e)
+        {
+            if (turn_player==false) // player 1
+            {
+                selected_panel = sender as Panel;
+
+                if (selected_panel.BackgroundImage == null)
+                {
+                    selected_panel.BackgroundImage = S1;
+                    check_win();
+                    turn_player = true;
+                }
+                else
+                {
+                    return;
+                }
+            }
+            else // player 2
+            {
+                selected_panel = sender as Panel;
+
+                if (selected_panel.BackgroundImage == null)
+                {
+                    selected_panel.BackgroundImage = S2;
+                    check_win();
+                    turn_player = false;
+                }
+                else
+                {
+                    return;
+                }
+            }
+        }
+    }
+}
